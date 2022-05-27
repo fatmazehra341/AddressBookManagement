010c36ff38d041ae54659e06cc574e23ea04512d
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-27 17:44:17 +0530

    [Fatma]:Adding multipal data in address book

--- AddressBookManagement/AddressBook.cs
+++ AddressBookManagement/AddressBook.cs
@@ -9,20 +9,46 @@ namespace AddressBookManagement
     internal class AddressBook
     {
 
+
         List<Contact> addressBook = new List<Contact>();
         public AddressBook()
         {
             Contact address1 = new Contact()
             {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                EmailAddress = Console.ReadLine(),
-                PostalCode = Convert.ToInt32(Console.ReadLine()),
-                MobileNumber = Convert.ToInt64(Console.ReadLine())
+                FirstName = "Fatma",
+                LastName = "Zehara",
+                Address = "Varanasi",
+                City = "Bangalore",
+                State ="UP",
+                EmailAddress = "fatma341gmail.com",
+                PostalCode = 221006,
+                MobileNumber = 854784588
             };
+            Contact address2 = new Contact()
+            {
+                FirstName = "Anita",
+                LastName = "Kumari",
+                Address = "Mumbai",
+                City = "Bangalore",
+                State = "UP",
+                EmailAddress = "fatma341gmail.com",
+                PostalCode = 221006,
+                MobileNumber = 854784588
+            };
+            addressBook.Add(address1);
+            addressBook.Add(address2);
         }
+        public void AddContactToAddressBook(Contact contact)
+        {
+            addressBook.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressBook)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address + "" + contact.City + " " + contact.State + " " + contact.EmailAddress + " " + " " + contact.PostalCode + " " + contact.MobileNumber);
+            }
+        }
+
     }
 }
