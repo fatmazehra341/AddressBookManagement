d8bf3b52604b32b4300e7df25bfd5cbbea738815
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-27 17:21:39 +0530

    [Fatma]:Displaying data in address book

---
+++ AddressBookManagement/AddressBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagement
+{
+    internal class AddressBook
+    {
+
+        List<Contact> addressBook = new List<Contact>();
+        public AddressBook()
+        {
+            Contact address1 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                EmailAddress = Console.ReadLine(),
+                PostalCode = Convert.ToInt32(Console.ReadLine()),
+                MobileNumber = Convert.ToInt64(Console.ReadLine())
+            };
+        }
+    }
+}
